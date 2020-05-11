22cf60783fdf26a1da5ccfd8e906a24db7233957
Kevin Porras <kporras07@gmail.com>
2020-05-11 16:35:56 -0600

    Update binary file and add version argument support.

---
+++ bin/chirripo-proxy.php
@@ -0,0 +1,29 @@
+<?php
+if (\file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+} elseif (\file_exists(__DIR__ . '/../../../autoload.php')) {
+    require_once __DIR__ . '/../../../autoload.php';
+}
+
+use Symfony\Component\Console\Application;
+use Console\App\Commands\ProxyUpCommand;
+use Console\App\Commands\ProxyDownCommand;
+
+/**
+ * Chirripo proxy version.
+ */
+function chirripo_proxy_version() {
+    return '1.2';
+}
+
+/**
+ * Chirripo proxy entrypoint.
+ */
+function chirripo_proxy_main() {
+    $app = new Application();
+    $app->setName('Chirripo Proxy Tool');
+    $app->add(new ProxyUpCommand());
+    $app->add(new ProxyDownCommand());
+
+    $app->run();
+}
